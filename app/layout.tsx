4cd87642348eef5bae6a15b3f0372285281b5db3
serhiyhvala <serhiyhvala.code@gmail.com>
2023-09-02 11:30:16 +0300

    feat: create header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 
+import Header from '@/components/Header'
+
 import './globals.css'
 
 const poppins = Poppins({
@@ -20,7 +22,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={poppins.className}>{children}</body>
+			<body className={poppins.className}>
+				<Header />
+				<main className='max-w-5xl mx-auto p-4'>{children}</main>
+			</body>
 		</html>
 	)
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Newspaper } from 'lucide-react'
+import Link from 'next/link'
+
+import AuthButtons from '../AuthButtons'
+
+export default function Header() {
+	return (
+		<header className='w-full border-b-2 border-gray-300'>
+			<div className='flex items-center justify-between p-4'>
+				<Link
+					href='/'
+					className='flex items-center gap-2 hover:text-blue-500 transition'
+				>
+					<Newspaper className='w-7 h-7' />
+					<span className='uppercase text-xl'>MiniBlog</span>
+				</Link>
+				<AuthButtons />
+			</div>
+		</header>
+	)
+}
