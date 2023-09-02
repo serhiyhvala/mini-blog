9f8ce573e745e12d30ca71ac0111f68aedbce63a
serhiyhvala <serhiyhvala.code@gmail.com>
2023-09-02 15:13:45 +0300

    chore: update types for database

--- types/database.types.ts
+++ types/database.types.ts
@@ -1,3 +1,6 @@
+export type Tables<T extends keyof Database['public']['Tables']> =
+	Database['public']['Tables'][T]['Row']
+
 export type Json =
 	| string
 	| number
@@ -6,12 +9,43 @@ export type Json =
 	| { [key: string]: Json | undefined }
 	| Json[]
 
-export type Tables<T extends keyof Database['public']['Tables']> =
-	Database['public']['Tables'][T]['Row']
-
 export interface Database {
 	public: {
 		Tables: {
+			post: {
+				Row: {
+					created_at: string
+					description: string | null
+					id: string
+					image: string | null
+					title: string | null
+					userId: string | null
+				}
+				Insert: {
+					created_at?: string
+					description?: string | null
+					id?: string
+					image?: string | null
+					title?: string | null
+					userId?: string | null
+				}
+				Update: {
+					created_at?: string
+					description?: string | null
+					id?: string
+					image?: string | null
+					title?: string | null
+					userId?: string | null
+				}
+				Relationships: [
+					{
+						foreignKeyName: 'post_userId_fkey'
+						columns: ['userId']
+						referencedRelation: 'user'
+						referencedColumns: ['id']
+					}
+				]
+			}
 			user: {
 				Row: {
 					created_at: string
