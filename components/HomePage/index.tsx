45e6dee4c95b895160d43fecbe6fe23bd853c57e
serhiyhvala <serhiyhvala.code@gmail.com>
2023-09-02 15:13:17 +0300

    feat: create home page component

---
+++ components/HomePage/index.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { PenSquare } from 'lucide-react'
+import Link from 'next/link'
+
+import { useUserProfile } from '@/hooks/useUserProfile'
+
+import { Button } from '../ui/button'
+import { Loader } from '../ui/loader'
+
+export default function HomePage() {
+	const { data, isLoading } = useUserProfile()
+	return isLoading ? (
+		<Loader />
+	) : (
+		<div className='flex flex-col gap-10'>
+			{data?.isWriter && (
+				<div className='flex items-center gap-5'>
+					<Button asChild>
+						<Link className='flex items-center gap-2' href='/new-post'>
+							<PenSquare className='w-5 h-5' />
+							<span>Write new post</span>
+						</Link>
+					</Button>
+					<Button>All Posts</Button>
+				</div>
+			)}
+		</div>
+	)
+}
