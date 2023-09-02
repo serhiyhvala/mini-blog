3157397f8d973a96e84232a06178b6668b14a505
serhiyhvala <serhiyhvala.code@gmail.com>
2023-09-02 15:12:12 +0300

    chore: update dependencies and next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
 				hostname: 'lh3.googleusercontent.com',
 				port: '',
 				pathname: '/**'
+			},
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				pathname: '/dsq7kf3kp/image/upload/**/**'
 			}
 		]
 	}
